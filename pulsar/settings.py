03c450927eead88bd508b1d56374830c6a91d38c
Hamzeh Mahmoudi <hamzehmahmoudi9@gmail.com>
2023-02-04 18:24:54 +0330

    add env

--- pulsar/settings.py
+++ pulsar/settings.py
@@ -104,7 +104,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(env('REDIS_HOST', '0.0.0.0'), 6379)],
+            "hosts": [(env('REDIS_HOST', 'localhost'), 6379)],
         },
     },
 }
